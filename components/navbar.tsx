15e030f8492d5b9ce0cb1e37d1c0627c20c29966
AcidicOnionas <104092113+AcidicOnionas@users.noreply.github.com>
2025-04-04 10:50:24 -0700

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,11 +20,11 @@ export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between">
-        <Link href="/" className="flex items-center space-x-2">
+        <Link href="/" className="flex items-center space-x-2 width">
         <img 
             src="/Logo.jpg" 
             alt="Chinese-Math Academy Logo" 
-            className="h-8 w-auto" 
+            className="h-24 w-auto" 
         />
         </Link>
 
